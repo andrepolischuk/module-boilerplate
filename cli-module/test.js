090f6065080192ab02c97cfe7eeb078087fa7bfd
andrepolischuk <andre.polischuk@gmail.com>
2016-01-23 22:38:38 +0300

    Tweaks

--- cli-module/test.js
+++ cli-module/test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import <%= camelName %> from './index.es5';
 
-test('should return `Hello world`', t => {
+test(t => {
   t.true(<%= camelName %>('world') === 'Hello world');
 });
